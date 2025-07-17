f53b600b54791b7c51d5ae67ff2733abd4bf331b
nahyun-shin <nahyun3645@naver.com>
2025-07-17 14:05:40 +0900

    init리스트

--- my-app07/src/pages/BlackJack.jsx
+++ my-app07/src/pages/BlackJack.jsx
@@ -5,19 +5,9 @@ import BlackJackComponents from './BlackJackComponents';
 
 
 
-const cardReducer=(state, action)=>{
-    switch(action.type){
-        case'start':
-            return {...state,cardList:action.payload};
-        case'reset':
-            return [];
-        default:
-            return state;
-    }
-}
 
 //셔플함수
-//안녕안녕
+
 const shuffle =(arr)=>{
     const arrNum=[...arr];
     for(let i=arrNum.length-1; i>0; i--){
@@ -27,21 +17,37 @@ const shuffle =(arr)=>{
     return arrNum; 
 }
 
-function BlackJack(props) {
-    const [state, dispatch]=useReducer(cardReducer,{ cardList: [] });
-    const[chacked, setChacked]=useState(0);
-    
-    const randomNum=()=>{
-        const arr = Array.from({length:20}, (_,i)=>i+1);
-        const shuffled = shuffle(arr);
+const initList={
+    cardList:[],
+    pcCardList:[],
+    checkedCards:[]
+}
 
-        //0~4번째 배열까지 잘라서 넣는다.
-        const cardList= shuffled.slice(0,5);
-        //5~9번째 배열까지 잘라서 넣는다.
-        const pcCardList=shuffled.slice(5,7);
-        console.log(cardList,pcCardList);
-        dispatch({type:'start', payload:Array.from(cardList)})    
+const cardReducer=(state, action)=>{
+    switch(action.type){
+        case'start':
+            const arr = Array.from({length:20}, (_,i)=>i+1);
+            const shuffled = shuffle(arr);
+            return {
+            //0~4번째 배열까지 잘라서 넣는다.
+            //5~9번째 배열까지 잘라서 넣는다.
+            cardList : shuffled.slice(0,5),
+            pcCardList : shuffled.slice(5,7),
+            checkedCards:[]
+        }
+        case'reset':
+            return initList;
+        default:
+            return state;
     }
+}
+
+
+function BlackJack(props) {
+    
+    const [state, dispatch]=useReducer(cardReducer,initList);
+
+    
 
     const choiceChk=()=>{
 
@@ -72,7 +78,7 @@ function BlackJack(props) {
                                     
                                         <input type="checkbox" 
                                             id='checkboxed'
-                                            onClick={(e) => handleCheck(cardNum)}
+                                            onClick={() => handleCheck(cardNum)}
                                             value={cardNum}/>
                                     
                                         {cardNum}
@@ -82,7 +88,7 @@ function BlackJack(props) {
                         }
                     </section>
                     <section className='btn-wrap'>
-                        <button type='button' onClick={randomNum}>시작</button>
+                        <button type='button' onClick={()=>dispatch({type:'start'})}>시작</button>
                         <button type='button' onClick={choiceChk}>선택</button>
                         <button type='button' onClick={'reset'}>리셋</button>
                     </section>
