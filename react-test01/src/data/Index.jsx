a0b391113ee05d96f8e4928b589fd07a897f18bd
nahyun-shin <nahyun3645@naver.com>
2025-07-28 17:21:57 +0900

    시험등록

---
+++ react-test01/src/data/Index.jsx
@@ -0,0 +1,8 @@
+export class Todo {
+    constructor (todoId, todoName, userName, checked){
+        this.todoId=todoId;
+        this.todoName=todoName;
+        this.userName=userName;
+        this.checked=checked;
+    }
+}
